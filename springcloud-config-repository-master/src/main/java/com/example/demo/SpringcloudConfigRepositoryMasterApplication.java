751448b98e7c897d956045d4f3140c6ad5ca181b
unknown <pardeepkamboj1992@gmail.com>
2020-09-29 12:12:54 +0530

    First commit

---
+++ springcloud-config-repository-master/src/main/java/com/example/demo/SpringcloudConfigRepositoryMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigRepositoryMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigRepositoryMasterApplication.class, args);
+	}
+
+}
